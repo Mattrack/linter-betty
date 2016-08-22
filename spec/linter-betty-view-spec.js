cf189d3169210798778f8bedea0231bdd4a6df54
Alexandre Gautier <alexandre.gautier@epitech.eu>
2016-08-22 03:04:12 -0700

    Package skeleton

---
+++ spec/linter-betty-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LinterBettyView from '../lib/linter-betty-view';
+
+describe('LinterBettyView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
